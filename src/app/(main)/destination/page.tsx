d620531ec88d303fc6f3a2ed00852503216f51b4
Herzinanda Putra <herzinanda31@gmail.com>
2025-11-12 18:38:41 +0700

    feat: create destination page

---
+++ src/app/(main)/destination/page.tsx
@@ -0,0 +1,16 @@
+import CTAbgSection from '@/component/main/destinations/CTAbgSection'
+import DestinationsSection from '@/component/main/destinations/DestinationsSection'
+import DestinationsSection2 from '@/component/main/destinations/DestinationsSection2'
+import React from 'react'
+
+const Destinations = () => {
+  return (
+    <>
+      <DestinationsSection2 />
+      <CTAbgSection />
+      <DestinationsSection />
+    </>
+  )
+}
+
+export default Destinations
